67e2f814240887fb3af24d845a5a86f6c5b43591
Pawan Kumar <ravishpk76@gmail.com>
2022-01-15 21:47:40 -0500

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Pawan Ravish on 2022-01-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
